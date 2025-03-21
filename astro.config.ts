fa1bc1be7804487dfe75ebca8667c4a0a7856f0f
Rory Hendrickson <35480205+roryhen@users.noreply.github.com>
2025-03-21 15:12:51 -0700

    upgrade deps

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs()],
 });
